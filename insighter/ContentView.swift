1eca075de1da8534e8f67c5a11bfe72c8ba6a6fa
Bruno Roque <brunoroque06@gmail.com>
2019-10-18 22:08:50 +0200

    Initial Commit

---
+++ insighter/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  insighter
+//
+//  Created by Bruno Roque on 18.10.19.
+//  Copyright © 2019 brunoroque06. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
