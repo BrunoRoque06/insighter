4d3f31e63a658bcac4cf8e62c92f18086f26f363
Bruno Roque <brunoroque06@gmail.com>
2019-10-23 00:20:30 +0200

    Clean test

--- insighterTests/spacesStateTests.swift
+++ insighterTests/spacesStateTests.swift
@@ -18,7 +18,6 @@ class spacesStateTests: XCTestCase {
     }
     
     func test_GivenConfigFile_WhenReading_ThenFieldsAreCorrect() {
-//        let path = FileManager.default.currentDirectoryPath + "/com.apple.spaces.plist"
         let path = "/Users/brunoroque/Projects/insighter/insighterTests/com.apple.spaces.plist"
         let url = URL(fileURLWithPath: path)
         let spaces = try! _readSpacesPlist(url: url)
@@ -26,24 +25,4 @@ class spacesStateTests: XCTestCase {
         XCTAssertEqual(spaces.spacesDisplayConfiguration.managementData.monitors[0].currentSpace?.uuid, "8C5D0BD7-D0F2-49AB-821F-C73507C21853")
         XCTAssertEqual(spaces.spacesDisplayConfiguration.spaceProperties.count, 3)
     }
-    
-//    func shell(_ command: String) -> String {
-//        let task = Process()
-//        task.launchPath = "/bin/bash"
-//        task.arguments = ["-c", command]
-//
-//        let pipe = Pipe()
-//        task.standardOutput = pipe
-//        task.launch()
-//
-//        let data = pipe.fileHandleForReading.readDataToEndOfFile()
-//        let output: String = NSString(data: data, encoding: String.Encoding.utf8.rawValue)! as String
-//
-//        return output
-//    }
-//    
-//    func test_cli() {
-//        let result = shell("yabai -m query --spaces")
-//        XCTAssertEqual(result, "SUP")
-//    }
 }
